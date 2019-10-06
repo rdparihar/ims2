d934317673a9dea6f85594cd725abee5b4fd5073
anitaparihar <anita.parihar.in@gmail.com>
2019-10-06 16:04:32 +0530

    Updated correct name

--- ims/ims/settings.py
+++ ims/ims/settings.py
@@ -81,14 +81,11 @@ WSGI_APPLICATION = 'ims.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bms_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
--- ims/ims/urls.py
+++ ims/ims/urls.py
@@ -42,4 +42,4 @@ urlpatterns = [
     path ('accounts/logout/', auth_views.LogoutView.as_view(), name="logout")
 
 ]
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
